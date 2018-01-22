976f4b969b4108b6c20575e309ec185445db3127
yeancarlosacapana <yeancarlos44@gmail.com>
2018-01-22 09:50:51 -0500

    update

--- config/constants.php
+++ config/constants.php
@@ -1,11 +1,11 @@
 <?php
 return[
     'hogaryspacios'=>[
-         //'url' => 'https://hogaryspacios.com'
-        'url'=>'http://127.0.0.1/hogaryspacios/'
+         'url' => 'https://hogaryspacios.com'
+        //'url'=>'http://127.0.0.1/hogaryspacios/'
     ],
     'images'=>[
-        // 'url' => 'http://apigarage.hogaryspacios.com/storage/'
-        'url'=>'http://127.0.0.1:8000/storage/'
+         'url' => 'http://apigarage.hogaryspacios.com/storage/'
+        //'url'=>'http://127.0.0.1:8000/storage/'
     ]
     ];
