d97b78a6a591c983efd8d40e6c0a0d9d70ed02c8
yeancarlosacapana <yeancarlos44@gmail.com>
2018-01-22 09:49:54 -0500

    update

--- app/Http/Controllers/customerController.php
+++ app/Http/Controllers/customerController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Customer;
-use App\Address;
+use App\customer;
+use App\address;
 use Carbon\Carbon;
 use DB;
 class customerController extends Controller
