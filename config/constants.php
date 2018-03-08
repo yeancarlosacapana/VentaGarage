b43cd9842c4305e71adabdf986248747e8e15f85
davidchoquerojas <choquerojasnoedavid@gmail.com>
2018-03-08 18:07:59 -0500

    add key secret to culqi

--- config/constants.php
+++ config/constants.php
@@ -8,5 +8,6 @@ return[
          'url' => 'http://apigarage.hogaryspacios.com/storage/'
         // 'url'=>'http://127.0.0.1:8000/storage/'
     ],
-    'secret_key_culqi' => 'sk_test_iVOFJoIQxdxVutk1'
+    //'secret_key_culqi' => 'sk_test_iVOFJoIQxdxVutk1'
+    'secret_key_culqi' => 'sk_live_NDYQgXmmIMBOOcen'
 ];
