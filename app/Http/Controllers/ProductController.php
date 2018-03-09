2494128466058f7be2302fa23fb5c1f06d207e45
davidchoquerojas <choquerojasnoedavid@gmail.com>
2018-03-09 12:09:40 -0500

    change ls

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -284,7 +284,7 @@ class ProductController extends Controller
                 $listImage[$key]->image = Config::get('constants.images.url').$image->id_image.'.jpg';
             }
             $listProduct->image = $listImage;
-            $listProduct->categoryProduct = CategoryProduct::where('id_product','=',$request["id_product"]);
+            $listProduct->categoryProduct = CategoryProduct::where('id_product','=',$request["id_product"])-get();
         }
         
         return response()->json($listProduct, 200);
